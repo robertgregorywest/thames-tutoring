357800d509f541ccde3896e41987f7282ffcc456
Rob West <robertgregorywest@gmail.com>
2020-09-09 15:48:52 +0100

    Started on contact form

---
+++ src/pages/contact.js
@@ -0,0 +1,110 @@
+/* eslint-disable no-undef */
+/* eslint-disable no-alert */
+import React from 'react';
+import { graphql } from 'gatsby';
+import { navigate } from 'gatsby-link';
+import { RichTextElement } from '@kentico/gatsby-kontent-components';
+import Layout from '../components/Layout';
+import '../assets/scss/pages/contact.scss';
+
+function encode(data) {
+  return Object.keys(data)
+    .map((key) => `${encodeURIComponent(key)}=${encodeURIComponent(data[key])}`)
+    .join('&');
+}
+
+const Contact = ({ data }) => {
+  const title = data.kontentItemArticle.elements.title.value;
+  const intro = data.kontentItemArticle.elements.body;
+
+  const [state, setState] = React.useState({});
+
+  const handleChange = (e) => {
+    setState({ ...state, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const form = e.target;
+    fetch('/', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+      body: encode({
+        'form-name': form.getAttribute('name'),
+        ...state,
+      }),
+    })
+      .then(() => navigate(form.getAttribute('action')))
+      .catch((error) => alert(error));
+  };
+
+  return (
+    <Layout title={title}>
+      <header className="page-head">
+        <h2>{title}</h2>
+        <RichTextElement value={intro.value} />
+      </header>
+      <div>
+        <form
+          name="contact"
+          method="post"
+          action="/thanks/"
+          data-netlify="true"
+          data-netlify-honeypot="bot-field"
+          onSubmit={handleSubmit}
+        >
+          <input type="hidden" name="form-name" value="contact" />
+          <p hidden>
+            <label htmlFor="bot-field">
+              Don’t fill this out:{' '}
+              <input name="bot-field" onChange={handleChange} />
+            </label>
+          </p>
+          <p>
+            <label htmlFor="name">
+              Your Name:{' '}
+              <input type="text" name="name" onChange={handleChange} />
+            </label>
+          </p>
+          <p>
+            <label htmlFor="email">
+              Your Email:{' '}
+              <input type="email" name="email" onChange={handleChange} />
+            </label>
+          </p>
+          <p>
+            <label htmlFor="subject">
+              Subject:{' '}
+              <input type="subject" name="subject" onChange={handleChange} />
+            </label>
+          </p>
+          <p>
+            <label htmlFor="message">
+              Message: <textarea name="message" onChange={handleChange} />
+            </label>
+          </p>
+          <p>
+            <button type="submit">Send</button>
+          </p>
+        </form>
+      </div>
+    </Layout>
+  );
+};
+
+export default Contact;
+
+export const pageQuery = graphql`
+  query ContactQuery {
+    kontentItemArticle(system: { codename: { eq: "contact" } }) {
+      elements {
+        title {
+          value
+        }
+        body {
+          value
+        }
+      }
+    }
+  }
+`;
---
+++ src/pages/thanks.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Layout from '../components/Layout';
+
+const Thanks = () => (
+  <Layout title="Thanks">
+    <div>
+      <p>Thanks</p>
+    </div>
+  </Layout>
+);
+
+export default Thanks;
