62ae1b8affe79916758df2efbd07222adb5fac38
Rob West <robertgregorywest@gmail.com>
2020-09-11 17:54:19 +0100

    Subject icons completed and colour palette simplified

--- src/components/SubjectAreasListing/index.js
+++ src/components/SubjectAreasListing/index.js
@@ -1,6 +1,19 @@
 import React from 'react';
 import './subjectAreasListing.scss';
 import abacus from '../../assets/icons/abacus.svg';
+import flask from '../../assets/icons/flask.svg';
+import book from '../../assets/icons/book-open.svg';
+
+function getIcon(subjectArea) {
+  switch (subjectArea) {
+    case 'science tuition':
+      return flask;
+    case 'english tuition':
+      return book;
+    default:
+      return abacus;
+  }
+}
 
 const SubjectAreasListing = ({ subjectAreas }) => (
   <div className="subject-areas-listing">
@@ -8,9 +21,14 @@ const SubjectAreasListing = ({ subjectAreas }) => (
     <div className="grid grid--gutters grid--full large-grid--fit">
       {subjectAreas &&
         subjectAreas.map((subject) => (
-          <div className="grid-cell">
+          <div className="grid-cell subject-areas-listing__cell">
             <div className="subject-areas-listing__icon">
-              <img src={abacus} width="60px" height="60px" alt="" />
+              <img
+                src={getIcon(subject.elements.title.value.toLowerCase())}
+                width="60px"
+                height="60px"
+                alt=""
+              />
             </div>
             <h3 className="subject-areas-listing__subject-title">
               {subject.elements.title.value}
@@ -18,7 +36,7 @@ const SubjectAreasListing = ({ subjectAreas }) => (
             <p>{subject.elements.summary.value}</p>
             <p>
               <a href="/" className="subject-areas-listing__link cta-secondary">
-                Learn more about {subject.elements.title.value.toLowerCase()}
+                Learn more about {subject.elements.title.value}
               </a>
             </p>
           </div>
