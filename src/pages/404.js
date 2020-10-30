ddcca306291ee2a28652fabb0aeaefc2959780a6
Rob West <robertgregorywest@gmail.com>
2020-10-30 08:24:22 +0000

    Add content to the 404 page

---
+++ src/pages/404.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Layout from '../components/Layout';
+
+const NotFoundRoute = () => (
+  <Layout>
+    <header className="page-head">
+      <h2>Page not found</h2>
+      <p>Page not found.</p>
+    </header>
+  </Layout>
+);
+
+export default NotFoundRoute;
