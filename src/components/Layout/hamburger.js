aa468c4d022836f9cb320cf98ebd2cf3a5ed0993
Rob West <robertgregorywest@gmail.com>
2020-11-17 09:36:13 +0000

    Addressing aria accessibility validation

--- src/components/Layout/hamburger.js
+++ src/components/Layout/hamburger.js
@@ -4,9 +4,8 @@ const Hamburger = ({ onClickHandler }) => (
   <a className="nav-burger" href="#" onClick={onClickHandler}>
     <div
       className="hamburger hamburger--collapse"
-      aria-label="Menu"
+      aria-label="Main menu"
       role="button"
-      aria-controls="navigation"
     >
       <div className="hamburger-box">
         <div className="hamburger-inner" />
