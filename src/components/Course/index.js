433e8fec02f95290134df5e4d1e8af7beef28e26
Rob West <robertgregorywest@gmail.com>
2020-12-02 17:25:30 +0000

    Course card updated to display subject

--- src/components/Course/index.js
+++ src/components/Course/index.js
@@ -4,8 +4,30 @@ import { RichTextElement } from '@kentico/gatsby-kontent-components';
 import Image from 'gatsby-image';
 import SkewWrapper from '../SkewWrapper';
 import './course.scss';
+import abacus from '../../assets/icons/abacus-red.svg';
+import flask from '../../assets/icons/flask-red.svg';
+import book from '../../assets/icons/book-open-red.svg';
+import dna from '../../assets/icons/dna.svg';
 import calendar from '../../assets/icons/calendar.svg';
 import pin from '../../assets/icons/location.svg';
+import atom from '../../assets/icons/atom.svg';
+
+function getIcon(subjectOption) {
+  switch (subjectOption.substr(0, subjectOption.indexOf(' ')).toLowerCase()) {
+    case 'biology':
+      return dna;
+    case 'chemistry':
+      return flask;
+    case 'physics':
+      return atom;
+    case 'science':
+      return flask;
+    case 'english':
+      return book;
+    default:
+      return abacus;
+  }
+}
 
 const formatDate = (date) =>
   new Date(date).toLocaleDateString('en-gb', {
@@ -19,6 +41,8 @@ const Course = ({ course }) => {
   const endDate = formatDate(course.elements.end_date.value);
   const courseDates =
     startDate === endDate ? startDate : `${startDate} - ${endDate}`;
+
+  const subject = course.elements.subject_option.value[0].elements.title.value;
   return (
     <div className="course">
       <div>
@@ -29,6 +53,17 @@ const Course = ({ course }) => {
         />
         <div>
           <h3 className="course__title">{course.elements.title.value}</h3>
+          <div className="course__details-grid">
+            <div>
+              <img
+                src={getIcon(subject)}
+                width="25px"
+                height="25px"
+                alt={subject}
+              />
+            </div>
+            <div className="course__details-text">{subject}</div>
+          </div>
           <div className="course__details-grid">
             <div>
               <img
